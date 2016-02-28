a7d33291903f48e8f1a150210a621d663417e3ca
Engr.Razibul Alam <razibul_alam@yahoo.com>
2016-02-28 21:00:30 +0600

    Basic PHP

---
+++ php/index.php
@@ -0,0 +1,40 @@
+<?php
+$heading = "<h1>PHP Basic</h1>";
+$paragraph = "<p>Heading Basic</p>";
+echo "<h1> $heading</h1>";
+echo "<p> $paragraph</P>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// phpinfo() ;
+// $a = 1;
+// var_dump($a);
+// $b = 1.1;
+// var_dump($b);
+// $c = "Hello";
+// var_dump($c);
+// $fruits = ['Apple','Banana','Orange'];
+// echo "<br>";
+// $fruit2 = array('Apple','Banana','Orange');
+// echo $fruits[2];
+// echo print_r($fruits);
+// echo "Hello Razib "; 
+// echo "<h1>You are in PHP World</h1> ";
+// echo "<p>You are a learner</p> ";
+// echo "<h1";
+// echo "<Hello";
+// echo "</h1";
+ ?>
